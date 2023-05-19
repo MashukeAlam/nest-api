f1f2bc7a03dfbef8d36dc40cd6a6003602bbc364
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-05-19 17:20:47 +0600

    Adding a Audio module to manage audio files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {AuthModule} from './auth/auth.module';
+import { AudiosModule } from './audios/audios.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AudiosModule],
   controllers: [AppController],
   providers: [AppService],
 })
