921a8454232e5e033b7194db6ba12f699a3a9d7f
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-05-19 17:32:37 +0600

    Generating route end points for audio module

--- src/audios/audios.controller.ts
+++ src/audios/audios.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('audios')
-export class AudiosController {}
+export class AudiosController {
+    // GET /
+    @Get()
+    getAll() {
+        return [];
+    }
+    // GET /:id
+    @Get(':id')
+    getOneAudio(@Param('id') id: string) {
+        return {
+            id
+        };
+    }
+    // DELETE /:id
+    // POST /
+    @Post()
+    uploadAudio(@Body())
+}
