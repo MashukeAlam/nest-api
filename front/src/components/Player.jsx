6f0ac22fc4125287823012266fa5f87bb0f2e8d3
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-05-21 18:10:42 +0600

    Added sliders

--- front/src/components/Player.jsx
+++ front/src/components/Player.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
 import Image from "./Image";
 import './styles/media.css';
 import { HiPlay, HiPause, HiArrowPathRoundedSquare } from "react-icons/hi2";
+const play = () => {
+    document.getElementById('player').play();
+}
+const pause = () => {
+    document.getElementById('player').pause();
+}
+
+
 const Player = () => {
+    const [progress, setProgress] = useState(0);
+    const setValues = (e) => {
+        const percentage = document.getElementById('player').currentTime / document.getElementById('player').duration;
+        setProgress(percentage * 300);
+    }
     return (
         <>
         <div className="all">
             <Image cover={"cover.jpg"}></Image>
             <div className="controls">
                 <div className="btn">
-                    <button><HiPlay /></button>
+                    <button onClick={play}><HiPlay /></button>
                 </div>
                 <div className="btn">
-                    <button><HiPause /></button>
+                    <button onClick={pause}><HiPause /></button>
                 </div>
                 <div className="btn">
                     <button><HiArrowPathRoundedSquare /></button>
                 </div>
             </div>
-            <audio controls src={"/sometimes.mp3"} />
+            <div className="slider">
+                <div id="top" className="top">
+                    <div style={{width: `${progress}px`}} id="bottom" className="bottom"></div>
+                </div>
+            </div>
+            <audio onTimeUpdate={setValues} id="player" controls src={"/sometimes.mp3"} />
         </div>
         </>
     )
